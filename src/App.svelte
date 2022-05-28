718f9a7eecd16d96cc167ecfcdfc261c3c3a03f4
elena shao <elenalingshao@gmail.com>
2022-05-28 14:19:11 -0700

    Add content component.

--- src/App.svelte
+++ src/App.svelte
@@ -2,28 +2,31 @@
 	/***************/
 	/*** IMPORTS ***/
 	/***************/
+
+	/* IMPORTING COMPONENTS */
 	import Map from "./Map.svelte";
-	import Grid from "./Header.svelte";
+	import Content from "./Content.svelte";
 </script>
 
 <svelte:head>
-	<!-- importing Google Fonts -->
-	<!-- CSS rules to specify the families:
-		font-family: 'IBM Plex Mono', monospace;
-		font-family: 'Space Grotesk', sans-serif;
-		font-family: 'Spectral', serif;
-		-->
+	<!-- IMPORTING GOOGLE FONTS -->
+	<!-- 
+			CSS rules to specify the families:
+			font-family: 'Chivo', sans-serif;
+			font-family: 'Marcellus', serif;
+			font-family: 'Spectral', serif;
+			font-family: 'Spline Sans Mono', monospace;
+	-->
 
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
-		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@400;500;600;700&family=Spectral:ital,wght@0,400;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Marcellus&family=Spectral:ital,wght@0,300;0,400;0,700;0,800;1,300;1,400;1,700;1,800&family=Spline+Sans+Mono:wght@300;500;700&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
 
-<Grid />
-<Map />
+<Content hed="Coal Plant Afterlives" byline="Elena Shao" />
 
 <style>
 	:global(body) {
---
+++ src/Content.svelte
@@ -0,0 +1,71 @@
+<script>
+	export let hed = "";
+	export let byline = "";
+	export let subhed = "";
+	export let content = "";
+</script>
+
+<section>
+	<div class="container">
+		{#if { hed } != ""}
+			<h1>{hed}</h1>
+		{/if}
+		{#if { byline } != ""}
+			<h2>By <a href="https://github.com/elenals">{byline}</a></h2>
+		{/if}
+		{#if { subhed } != ""}
+			<h3>{subhed}</h3>
+		{/if}
+		{#if { content } != ""}
+			<p>{content}</p>
+		{/if}
+	</div>
+</section>
+
+<style>
+	section {
+		text-align: center;
+		width: 100vw;
+		overflow: hidden;
+	}
+
+	h1 {
+		text-align: center;
+		font-family: "Chivo", sans-serif;
+		font-size: 50px;
+		font-weight: 900;
+	}
+
+	h2 {
+		text-align: center;
+		font-family: "Spline Sans Mono", monospace;
+		font-size: 18px;
+		font-weight: 400;
+		text-transform: uppercase;
+	}
+
+	a {
+		display: inline-block;
+		position: relative;
+		color: rgb(46, 46, 46);
+		text-decoration: none;
+	}
+
+	a:after {
+		content: "";
+		position: absolute;
+		width: 100%;
+		transform: scaleX(0);
+		height: 3px;
+		left: 0;
+		bottom: 0;
+		background-color: rgb(46, 46, 46);
+		transform-origin: bottom right;
+		transition: transform 0.25s ease-out;
+	}
+
+	a:hover:after {
+		transform: scaleX(1);
+		transform-origin: bottom left;
+	}
+</style>
