35fa9d221ba75e2f1cdca813f3147a323098e8bb
elena shao <elenalingshao@gmail.com>
2022-05-28 19:49:56 -0700

    Add content component.

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-	export let name;
+	/* IMPORTS */
+	import Content from "./Content.svelte"; // reusable heading & content component
+	import Map from "./Map.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head>
+	<!-- importing Google Fonts -->
+	<!-- 
+			CSS rules to specify the families:
+			font-family: 'Chivo', sans-serif;
+			font-family: 'Fira Code', monospace;
+			font-family: 'Marcellus', serif;
+			font-family: 'Spectral', serif;
+	-->
+
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Fira+Code:wght@300;500;700&family=Marcellus&family=Spectral:ital,wght@0,300;0,400;0,700;0,800;1,300;1,400;1,700;1,800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<Content
+	hed="Coal Plant Afterlives"
+	byline="Elena Shao"
+	content="<p>Across the country, coal-fired power plants like TKTK are powering down, squeezed out by cheaper natural gas and renewable energy and tougher emissions regulations. The pattern, while promising for a greener future, also ends up leaving behind a toxic legacy of industrial pollution and waste. In coal dependent communities, the transition away from coal can disrupt families and economies.</p>"
+/>
+
+<Map />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	:global(body) {
+		margin: 0px;
+		padding: 0px;
 	}
 </style>
